1237fe472d396b2c49494cb374e214d75409dad1
David <2018dpollack@gmail.com>
2023-10-23 11:56:10 -0700

    Add NavBar and mainPage

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+function MainPage() {
+    return (
+      <>
+        <p>Rocky Road Trip</p>
+        <footer>
+          Copyright &copy; 2023 Rocky Road Trip
+        </footer>
+      </>
+    );
+}
+  
+export default MainPage;
