2b22777743b0d748954c8f26b5c882f83bf8b6eb
swathi <swathibalasubramanyam@gmail.com>
2023-10-23 11:30:22 -0700

    express error handling

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
 
 const cors = require('cors');
 const { isProduction } = require('./config/keys');
@@ -42,5 +43,28 @@ app.use('/api/session', sessionRouter);
 app.use('/api/itineraries', itinerariesRouter);
 app.use('/api/reviews', reviewsRouter);
 app.use('/api/csrf', csrfRouter);
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
 
 module.exports = app;
