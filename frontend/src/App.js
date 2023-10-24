a7d08353d435c4a813d7b19a20b9dc576117d422
Eric-Mai-25 <ericmai2@gmail.com>
2023-10-23 21:24:11 -0700

    Made a map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar/NavBar';
 import MainPage from './components/MainPage/MainPage';
 import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
+import SplashPage from './components/SplashPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <NavBar />
       <Switch>
         <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/Splash" component={SplashPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
       </Switch>
