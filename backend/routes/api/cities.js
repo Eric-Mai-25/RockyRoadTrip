3afcf84ad006b8229da089269528d7e96298f31c
swathi <swathibalasubramanyam@gmail.com>
2023-10-25 15:06:14 -0700

    Update cities get endpoint to return an object

--- backend/routes/api/cities.js
+++ backend/routes/api/cities.js
@@ -5,19 +5,13 @@ const City = mongoose.model('City');
 
 router.get('/', async(req, res) => {
     const cities = await City.find();
-    return res.json(cities);
-})
 
-router.get('/:lng/:lat', async(req, res) => {
-    const METERS_PER_MILE = 1609.34
-    const cities = 
-        await City.count({ 
-            location: { 
-                $nearSphere: { 
-                    $geometry: { type: "Point", 
-                                coordinates: [ req.params.lng, req.params.lat ] }, 
-                    $maxDistance: 10000 * METERS_PER_MILE } } })
-    return res.json(cities)
+    let newCitiesObj = {}
+    cities.forEach((city) => {
+        newCitiesObj[city._id] = city._doc
+    })
+
+    return res.json(newCitiesObj);
 })
 
 module.exports = router;
